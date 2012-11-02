add1dac7bbfb4848f60ea2b2a25fcacd0161a825
Carsten Senkfeil <noheadlights@me.com>
2012-11-03 00:47:15 +0100

    product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
-  validates :description, presence: true
+  validates :description, presence: true, length: {minimum: 10}
   validates :title, presence: true, uniqueness: true
   validates :image_url, presence: true, format: {with: %r{\.(gif|jpg|png)\Z}i,
                                                  message: 'must be a URL for GIF,JPG or PNG.'}
